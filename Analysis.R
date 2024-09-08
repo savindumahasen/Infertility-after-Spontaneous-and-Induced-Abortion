8aff2e1f6b90a124e10d173a9f4fc14708d45336
Savindu <savinduruhunuhewa@gmail.com>
2024-09-08 15:38:32 +0530

    Complete the normality testing

--- Analysis.R
+++ Analysis.R
@@ -10,15 +10,46 @@ install.packages("nortest")
 ## call the library
 library("nortest")
 
-## anderson darling testing for Education
-#d.test(case)
-#illie.test(case)
-#sapiro.test(case)
+## Normality testing for Education
+#ad.test(education)
+#lillie.test(education)
+#shapiro.test(education)
 
-## anderson darling testing for age
-#ad.test(age)
-#lillie.test(age)
-#shapiro.test(age)
+## Normality testing for age
+ad.test(age)
+lillie.test(age)
+shapiro.test(age)
+
+## Normality testing for parity
+ad.test(parity)
+lillie.test(parity)
+shapiro.test(parity)
+
+## Normality testing for induced
+ad.test(induced)
+lillie.test(induced)
+shapiro.test(induced)
+
+## Normality testing for case
+ad.test(case)
+lillie.test(case)
+shapiro.test(case)
+
+## Normality testing for spontaneous
+ad.test(spontaneous)
+lillie.test(spontaneous)
+shapiro.test(spontaneous)
+
+## Normality testing for stratum
+ad.test(stratum)
+lillie.test(stratum)
+shapiro.test(stratum)
+
+## Normality testing for pooled.stratum
+ad.test(pooled.stratum)
+lillie.test(pooled.stratum)
+shapiro.test(pooled.stratum)
+
 ## install the package neuralnet
 #install.packages("neuralnet")
 
