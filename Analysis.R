e7957dec98f4b3b6f2bd8286351c4adc584da565
Savindu <savinduruhunuhewa@gmail.com>
2024-09-08 15:07:05 +0530

    Attach the dataset

--- Analysis.R
+++ Analysis.R
@@ -1,6 +1,24 @@
+## attach the dataset
+attach(infert)
 
+
+
+## Normality testing
+#install.packages("nortest")
+## call the library
+#library("nortest")
+
+## anderson darling testing for Education
+#d.test(case)
+#illie.test(case)
+#sapiro.test(case)
+
+## anderson darling testing for age
+#ad.test(age)
+#lillie.test(age)
+#shapiro.test(age)
 ## install the package neuralnet
-install.packages("neuralnet")
+#install.packages("neuralnet")
 
 ## call the library
 library(neuralnet)
@@ -9,3 +27,8 @@ library(neuralnet)
 ## check the observation
 dim(infert)
 
+?neuralnet
+
+
+#nn= neuralnet(case~age+parity+induced+spontaneous, data=infert, hidden=2,
+ #             err.fct = "ce",linear.output=FALSE)
